c5c2d771a8dbcf1f953fc40e669296cc05635d21
Racheli-BP <4141893@gmail.com>
2023-10-17 15:08:42 +0300

    arrange the project

---
+++ SmartEye/Tests/Test.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+// #include "catch.hpp"
+
+// TEST_CASE("Example Test Case", "[example]") {
+//     REQUIRE(2 + 2 == 4);
+//     REQUIRE(3 * 4 == 12);
+// }
+
+int main(){
+    return 1;
+}
